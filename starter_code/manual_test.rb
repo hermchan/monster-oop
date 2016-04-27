308edcdeea5baa34b7501cd720bb119a711d2135
Herman Chan <hermchan89@gmail.com>
2016-04-26 19:54:56 -0700

    completed monster

--- starter_code/manual_test.rb
+++ starter_code/manual_test.rb
@@ -46,47 +46,47 @@ puts "name of rabbit is #{rabbit.name} (Rabbit of Caerbannog)"  # getter
 # name of rabbit is Rabbit of Caerbannog
 
 puts "\nteletubby (medium) vs dalek (high) (test Monster.fight)"
-# dalek.name = "dalek"
-# teletubby.name = "teletubby"
-# puts "winner is #{Monster.fight(teletubby, dalek).name} (dalek)"
+dalek.name = "dalek"
+teletubby.name = "teletubby"
+puts "winner is #{Monster.fight(teletubby, dalek).name} (dalek)"
 
 puts "\nteletubby (medium) vs rabbit (medium) (test Monster.fight)"
-# puts "winner is #{Monster.fight(teletubby, rabbit).name} (Rabbit of Caerbannog)"
+puts "winner is #{Monster.fight(teletubby, rabbit).name} (Rabbit of Caerbannog)"
 
 #### INHERITANCE ####
 puts "\n#### INHERITANCE ####"
 
 puts "\nRob Zombie (test Zombie initialize)"
-# rob = Zombie.new
-# # Rawr!
-# # 5 monsters now roam the world!
-# rob.name = "Rob Zombie"	# also testing name getter and setter method inheritance
-# puts "#{rob.name}'s habitat is #{rob.habitat} (graveyard)"
-# # Rob Zombie's habitat is graveyard
-# puts "#{rob.name}'s threat level is #{rob.threat_level} (medium)"
-# # Rob Zombie's threat level is medium
+rob = Zombie.new
+# Rawr!
+# 5 monsters now roam the world!
+rob.name = "Rob Zombie"	# also testing name getter and setter method inheritance
+puts "#{rob.name}'s habitat is #{rob.habitat} (graveyard)"
+# Rob Zombie's habitat is graveyard
+puts "#{rob.name}'s threat level is #{rob.threat_level} (medium)"
+# Rob Zombie's threat level is medium
 
 puts "\nteen_wolf (test Werewolf initialize)"
-# teen_wolf = Werewolf.new
-# # Rawr!
-# # 6 monsters now roam the world!
-# puts "teen_wolf's threat_level is #{teen_wolf.threat_level} (low)"
-# #teen_wolf's threat_level is low
+teen_wolf = Werewolf.new
+# Rawr!
+# 6 monsters now roam the world!
+puts "teen_wolf's threat_level is #{teen_wolf.threat_level} (low)"
+#teen_wolf's threat_level is low
 
 puts "\nteen_wolf full moon (test Werewolf#update_threat_level)"
-# puts "when full_moon is true, threat level returns #{teen_wolf.update_threat_level(true)} (midnight)"
-# # when full_moon is true, threat level is midnight
-# puts "... threat_level is set to #{teen_wolf.threat_level} (midnight)"
-# # threat_level is set to midnight
-# puts "when full_moon is false, threat level returns #{teen_wolf.update_threat_level(false)} (low)"
-# # when full_moon is false, threat level is low
-# puts "... threat_level is set to #{teen_wolf.threat_level} (low)"
-# # threat_level is set to low
+puts "when full_moon is true, threat level returns #{teen_wolf.update_threat_level(true)} (midnight)"
+# when full_moon is true, threat level is midnight
+puts "... threat_level is set to #{teen_wolf.threat_level} (midnight)"
+# threat_level is set to midnight
+puts "when full_moon is false, threat level returns #{teen_wolf.update_threat_level(false)} (low)"
+# when full_moon is false, threat level is low
+puts "... threat_level is set to #{teen_wolf.threat_level} (low)"
+# threat_level is set to low
 
 puts "\n(test class descriptions)"
-# puts "Monster class: #{Monster.class_description}"
-# puts "Zombie class: #{Zombie.class_description}"
-# puts "Werewolf class: #{Werewolf.class_description}"
+puts "Monster class: #{Monster.class_description}"
+puts "Zombie class: #{Zombie.class_description}"
+puts "Werewolf class: #{Werewolf.class_description}"
 
 #### MODULES ####
 puts "\n#### MODULES ####"
--- starter_code/monster.rb
+++ starter_code/monster.rb
@@ -1,25 +1,26 @@
 # Monster class
 class Monster
   # getters and setters for instance variables
-  attr_accessor :threat_level, :habitat, :name
+  attr_accessor :threat_level, :habitat, :name, :fight
   # class variable for count
   @@count = 0
+
   # class instance variable for class description
-  def initialize(threat_level="medium")
-    @threat_level = threat_level
-    p "Rawr"
-    @@count += 1
-  end
-
-
-
+  @class_description = "Four legged freaks"
   # class method getter for @@count class variable
   def self.count
     @@count
   end
   # class method getter for @class_description class instance variable
-
+  def self.class_description
+    @class_description
+  end
   # initial behavior
+  def initialize(threat_level="medium")
+    @threat_level = threat_level
+    p "Rawr"
+    @@count += 1
+  end
 
   # habitat? instance method
   def habitat?(habitat)
@@ -30,22 +31,47 @@ class Monster
   # get_dangerous instance method
 
   # fight class method
-
-
+  def self.fight(monster1, monster2)
+    if monster1.threat_level == monster2.threat_level
+      monster2
+    elsif monster1.threat_level == "medium" && monster2.threat_level == "high"
+      monster2
+    else
+      monster1
+    end
+  end
+end
 # Zombie class
+class Zombie < Monster
 
   # zombie version of class_description
-
+  @class_description = "Rotten fresh and missing an arm, both legs, and both eyes."
   # initial behavior and values
+  def initialize
+    @habitat = "graveyard"
+  end
+end
 
 # Werewolf class
+class Werewolf < Monster
+
 
   # werewolf version of class_description
-
+  @class_description = "I am Jacob from Twilight and I am a wolf"
   # initial behavior and values
+  def initialize(threat_level="low")
+    @threat_level = threat_level
+  end
 
   # update_threat_level instance method
-
+  def update_threat_level(full_moon)
+    if full_moon == true
+      @threat_level = "midnight"
+    else
+      @threat_level = "low"
+    end
+    @threat_level
+  end
 # Flying module
 
   #fly method
